ab14c68d119b123a60f7a91eb884e86fa5b0e183
Kateryna Shydlovska <katerynashydlovska@Katerynas-MacBook-Air.local>
2021-06-05 11:53:31 -0700

    first java program

---
+++ javaFun/FirstJavaProgram.java
@@ -0,0 +1,10 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        String name = "Kate";
+        String age = "27";
+        String city = "Austin";
+        System.out.println("My name is " + name);
+        System.out.println("I am " + age);
+        System.out.println("My home town " + city);
+    }
+}
