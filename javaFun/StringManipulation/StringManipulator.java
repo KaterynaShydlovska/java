2dcd1e07c9764f88d40437ad9199e46669a4d0f9
Kateryna Shydlovska <katerynashydlovska@Katerynas-MacBook-Air.local>
2021-06-07 13:15:29 -0500

    finished string manipulation

--- javaFun/StringManipulation/StringManipulator.java
+++ javaFun/StringManipulation/StringManipulator.java
@@ -3,4 +3,18 @@ public class StringManipulator{
         String newStr = s.trim() + s1.trim();
         return newStr;
     }
+
+    public Integer getIndexOrNull(String str, char c){
+        int res = str.indexOf(c);
+        return res == -1 ? null : res;
+    }
+
+    public Integer getIndexOrNull(String word, String subString){
+        int res = word.indexOf(subString);
+        return res == -1 ? null : res;
+    }
+
+    public String concatSubstring(String stringOne, int indexOne, int indexTwo, String stringTwo){
+        return stringOne.substring(indexOne,indexTwo) + stringTwo;
+    }
 }
--- javaFun/StringManipulation/StringManipulatorTest.java
+++ javaFun/StringManipulation/StringManipulatorTest.java
@@ -4,5 +4,24 @@ public class StringManipulatorTest {
         String str = manipulator.trimAndConcat("    Hello     ","     World    ");
         System.out.println(str); // HelloWorld 
 
+        char letter = 'o';
+        Integer a = manipulator.getIndexOrNull("Coding", letter);
+        Integer b = manipulator.getIndexOrNull("Hello World", letter);
+        Integer c = manipulator.getIndexOrNull("Hi", letter);
+        System.out.println(a); // 1
+        System.out.println(b); // 4
+        System.out.println(c); // null
+
+        String word = "Hello";
+        String subString = "llo";
+        String notSubString = "world";
+        Integer str1 = manipulator.getIndexOrNull(word, subString);
+        Integer str2 = manipulator.getIndexOrNull(word, notSubString);
+        System.out.println(str1); // 2
+        System.out.println(str2); // null
+
+        String wordToConcat = manipulator.concatSubstring("Hello", 1, 2, "world");
+        System.out.println(wordToConcat); // eworld
+
     }
 }
