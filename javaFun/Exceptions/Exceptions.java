fc823f3d97c54c17591531ccd777bb5a755164ab
Kateryna Shydlovska <katerynashydlovska@Katerynas-MacBook-Air.local>
2021-06-07 22:28:58 -0500

    exceptions

---
+++ javaFun/Exceptions/Exceptions.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Exceptions {
+    public static void main(String[] args) {
+        List<Object> myList = new ArrayList<Object>();
+        myList.add("13");
+        myList.add("hello world");
+        myList.add(48);
+        myList.add("Goodbye World");
+        try{
+            for(int i = 0; i < myList.size(); i++) {
+                Integer castedValue = (Integer) myList.get(i);
+            }  
+        } catch (ClassCastException e){   
+            System.out.println("OOOPs, something went wrong");
+            }  
+            
+        }
+    }
