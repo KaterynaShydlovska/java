b45a20195ae224f37b1b7116aada4f2201e73f76
Kateryna Shydlovska <katerynashydlovska@Katerynas-MacBook-Air.local>
2021-06-08 22:03:07 -0500

    zooOne project with spring

---
+++ OOP/ZooOne/src/com/oopfundamentals/zoo/GorillaTest.java
@@ -0,0 +1,21 @@
+package com.oopfundamentals.zoo;
+
+public class GorillaTest {
+
+	public static void main(String[] args) {
+		Gorilla gorilla = new Gorilla();
+		gorilla.throwSomething(5);
+		gorilla.throwSomething(5);
+		gorilla.throwSomething(5);
+		
+		gorilla.eatBananas(10);
+		gorilla.eatBananas(10);
+		
+		gorilla.climb(10);
+		
+		gorilla.displayEnergy();
+		
+
+	}
+
+}
---
+++ OOP/ZooOne/src/com/oopfundamentals/zoo/Mammal.java
@@ -0,0 +1,17 @@
+package com.oopfundamentals.zoo;
+
+public class Mammal {
+	private int energyLevel;
+	
+	public Mammal() {
+		this.energyLevel = 100;
+	}
+	
+	public int displayEnergy() {
+		System.out.println(this.energyLevel);
+		return this.energyLevel;
+		}
+	public void setEnergy(int en) {
+		this.energyLevel = en;
+	}
+	}
