123f4ed6555857b6e9792d5db26c6f6c3e72d4d6
Kateryna Shydlovska <katerynashydlovska@Katerynas-MacBook-Air.local>
2021-06-09 14:53:58 -0500

    oblject master project

---
+++ OOP/ObjectMasterOne/src/com/javaoopfund/objectmaster/HumanTest.java
@@ -0,0 +1,13 @@
+package com.javaoopfund.objectmaster;
+
+public class HumanTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Human personOne = new Human();
+		Human personTwo = new Human();
+		System.out.println(personOne.attack(personTwo));
+		
+	}
+
+}
