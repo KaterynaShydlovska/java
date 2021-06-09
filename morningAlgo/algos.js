9255916436761a1f3b2c636f5e9d23a20a3deced
Kateryna Shydlovska <katerynashydlovska@Katerynas-MacBook-Air.local>
2021-06-09 09:18:31 -0500

    binary search

--- morningAlgo/algos.js
+++ morningAlgo/algos.js
@@ -49,3 +49,39 @@ console.log(Balancepoint([4, 7, 1, 3])) //false
 // total = 20
 // leftsum = 10
 // remaining(total-leftsum) -> 10
+
+function binarySearch(arr, value) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        let mid = Math.ceil((left + right) / 2)
+        if (arr[mid] === value) {
+            return true;
+        } else if (arr[mid] < value) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return false;
+}
+
+// binarySearch([5,8,9,15,17,23,24,34,77], 60) //false
+// binarySearch([5,8,9,15,17,23,24,34,77], 34) //true
+
+
+function recursiveBS(arr, x, start = 0, end = arr.length - 1) {
+    if (start > end) return false;
+
+    let mid = Math.floor((start + end) / 2);
+
+    if (arr[mid] === x) return true;
+
+    if (arr[mid] > x)
+        return recursiveBS(arr, x, start, mid - 1);
+    else
+        return recursiveBS(arr, x, mid + 1, end);
+}
+
+recursiveBS([5, 8, 9, 15, 17, 23, 24, 77], 34)
