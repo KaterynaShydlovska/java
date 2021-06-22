a995bd7cef573397f6b5c839ab12421c6b7254f5
Kateryna Shydlovska <shydlovskayaka@gmail.com>
2021-06-22 10:37:07 -0500

    bst find max and min

--- morningAlgo/algos.js
+++ morningAlgo/algos.js
@@ -491,3 +491,106 @@ class DLL {
     bst1.add(10)
     bst1.add(11)
     console.log(bst1.add(20));
+
+
+    class Node {
+        constructor(value) {
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+
+    class BST {
+        constructor() {
+            this.root = null;
+        }
+
+        add(value) {
+            //create a node
+            let newNode = new Node(value)
+            if (this.root == null) {
+                this.root = newNode;
+            } else {
+                let runner = this.root;
+                while (runner != null) {
+                    if (value > runner.value) {
+                        if (runner.right == null) {
+                            runner.right = newNode;
+                            break
+                        } else {
+                            runner = runner.right
+                        }
+                    } else if (value < runner.value) {
+                        if (runner.left == null) {
+                            runner.left = newNode;
+                            break
+                        } else {
+                            runner = runner.left
+                        }
+                    } else {
+                        console.log("no duplicates allowed in a BST")
+                        return this;
+                    }
+                }
+            }
+            return this;
+        }
+
+
+        findMin() {
+            let min = this.root.value;
+            if (this.root.left) {
+                let runner = this.root.left;
+                while (runner) {
+                    // console.log(runner, 'here')
+                    if (runner.value < min) {
+                        min = runner.value;
+                        runner = runner.left;
+                    }
+                }
+
+            }
+            return min;
+        }
+
+
+
+
+
+        findMax() {
+            let max = this.root.value;
+            if (this.root.right) {
+                let runner = this.root.right;
+                while (runner) {
+                    if (runner.value > max) {
+                        max = runner.value;
+                        runner = runner.right;
+                    }
+                }
+            }
+            return max;
+
+        }
+
+        printTree(node) {
+            console.log("Current node = ", node.value);
+            if (node.left != null) {
+                console.log("left node of", node.value, "= " + node.left.value)
+                this.printTree(node.left)
+            }
+            if (node.right != null) {
+                console.log("Right node of", node.value, " = " + node.right.value)
+                this.printTree(node.right);
+            }
+        }
+    }
+
+
+
+    let bst1 = new BST()
+    bst1.add(12).add(15).add(10).add(11).printTree(bst1.root)
+    console.log('----------')
+    console.log(bst1.findMin());
+    console.log(bst1.findMax());
