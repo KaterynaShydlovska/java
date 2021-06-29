84742d4dab349379c0fe64202b3e91e6f7f02d61
Kateryna Shydlovska <shydlovskayaka@gmail.com>
2021-06-29 15:35:40 -0500

    tries tree

--- morningAlgo/algos.js
+++ morningAlgo/algos.js
@@ -698,3 +698,61 @@ class DLL {
     console.log('----------')
     console.log(bst1.findMin());
     console.log(bst1.findMax());
+
+
+    class TrieNode {
+        constructor(letter) {
+            this.letter = letter;
+            this.children = {};
+            this.isWord = false
+        }
+    }
+
+    class TrieSet {
+        constructor() {
+            this.root = new TrieNode("")
+        }
+        add(str) {
+            //create a runner that allows us to navigate ourselves down this trie
+            let runner = this.root;
+            //loop through every character in the string
+            for (let i = 0; i < str.length; i++) {
+                // console.log(str[i])
+                //if the children of the current node we are on does not contain a key for the letter we want to reference, then we need to create a new node that contains that letter and assign that node to this particular nodes children
+                if (!runner.children.hasOwnProperty(str[i])) {
+                    runner.children[str[i]] = new TrieNode(str[i]);
+                }
+                //move runner to the node for that new letter that got inserted
+                runner = runner.children[str[i]]
+            }
+            //at this point, we have runner at the node that represents the end of a word/term 
+            runner.isWord = true
+
+        }
+
+        printAllWords(root = this.root, str = "") {
+            //base case
+            if (root.isWord) {
+                console.log(str)
+            }
+
+            //forward progress--> recurse through each child node, while passing a string that we build along the way
+            for (const key in root.children) {
+                this.printAllWords(root.children[key], str + key)
+            }
+        }
+
+
+
+
+    }
+
+
+
+    let words = new TrieSet();
+    words.add("cab")
+    words.add("can")
+    words.add("cog")
+    words.printAllWords()
+        // words.add("dog")
+        // words.add("cat")
