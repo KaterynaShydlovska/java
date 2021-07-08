927dbe3c649ab6b0b11a1d00fb958be62c9ed899
Kateryna Shydlovska <shydlovskayaka@gmail.com>
2021-07-08 12:08:54 -0500

    partition array

--- morningAlgo/algos.js
+++ morningAlgo/algos.js
@@ -908,5 +908,46 @@ function selectionSort2(arr){
 
 console.log(selectionSort2([5,4,2,6,8,14,1,3]));
 
+//partition the array around the value at arr[0]
+//without creating a new array
+//values left of the original pivot should be less than the pivot
+//values right of the pivot should be >= the pivot value
+//note: each side of the partitioned value does not
+//have to be sorted.
+//return the newly partitioned array
+//bonus: return the index where the value at 0 ends up
+//[5,4,9,2,5,3] -> [4,2,3,5,9,5], return 3
+const partition = (arr) => {
+    let pivot = arr[0];
+    let pointer = 0;
+
+    for(let i = 1; i < arr.length; ++i){
+        //if arr[i] < pivot
+        if(arr[i] < pivot){
+            ++pointer;
+
+            let temp = arr[pointer];
+            arr[pointer] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    let temp = arr[0];
+    arr[0] = arr[pointer];
+    arr[pointer] = temp;
+        
+    console.log(arr);
+    return pointer;
+
+}
+
+//should return [4,2,3,5,9,5]
+console.log(partition([5,4,9,2,5,3]));
+//should return [4,7]
+console.log(partition([7,4]));
+//should return [2,1,3,4,5]
+console.log(partition([3,5,1,2,4]));
+
+console.log(partition([7,2,10,15,1]));
+
 
 
